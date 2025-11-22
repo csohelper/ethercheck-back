33b5f4249c0a3ee58a02d9ca0900fc2632db415f
Slava Petrov <slavapmk@gmail.com>
2025-11-22 13:31:57 +0000

    Certbot

--- init-key.bash
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-docker compose run --rm certbot certonly --webroot --webroot-path /var/www/certbot/ -d monitor.slavapmk.ru
-
-# https://dev.to/mindsers/https-using-nginx-and-lets-encrypt-in-docker-1ama
